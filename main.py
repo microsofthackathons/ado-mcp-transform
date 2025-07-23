9f3d056dc9ece71b015e311ec0981060f9bca11e
Avi Levin <avilevin@microsoft.com>
2025-07-23 10:17:04 +0300

    Demonstrate processing content

--- main.py
+++ main.py
@@ -4,9 +4,15 @@
 # ]
 # ///
 
+from typing import Literal
 from fastmcp import FastMCP
 from fastmcp.tools.tool import Tool, ToolResult
 from fastmcp.tools.tool_transform import forward
+from mcp.types import TextContent
+import asyncio
+import os
+import json
+
 
     # Create a proxy directly from a config dictionary
 config = {
@@ -24,9 +30,18 @@ proxy = FastMCP.as_proxy(config, name="ADO MCP Proxy")
 
 async def custom_output(**kwargs) -> ToolResult:
     result = await forward(**kwargs)
-    return ToolResult(structured_content={"Foo": "Bar", "original": result.content})  # Custom output structure
+    if (
+        len(result.content) == 1
+        and isinstance(result.content[0], TextContent)
+    ):
+        try:
+            parsed = json.loads(result.content[0].text)
+            return ToolResult(structured_content=parsed)
+        except Exception:
+            pass
+    return result
 
-async def main():
+async def main(transport: Literal["stdio", "sse"] = "stdio"):
     """Setup the proxy and configure tools"""
     
     original_tool = (await proxy.get_tool("repo_get_repo_by_name_or_id")).copy()
@@ -41,10 +56,11 @@ async def main():
     
     original_tool.disable()  # Disable the mirrored tool in the proxy
 
-    await proxy.run_async()
+    await proxy.run_async(transport=transport)
 
 # Run the server with stdio transport for local access
 if __name__ == "__main__":
-
-    import asyncio
-    asyncio.run(main())  # Setup the proxy and tools
+    transport = os.environ.get("MCP_TRANSPORT", "stdio")
+    if transport not in ("stdio", "sse"):
+        transport = "stdio"
+    asyncio.run(main(transport))  # Setup the proxy and tools
